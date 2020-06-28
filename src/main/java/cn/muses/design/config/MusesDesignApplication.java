96cb6ce14e5ca81273f4ff3a29ae6c96d8fa0c5c
miaoqiang <miaoqiang@xyz.cn>
2020-06-28 09:43:22 +0800

    类名变更

--- src/main/java/cn/muses/design/config/MusesDesignApplication.java
+++ src/main/java/cn/muses/design/config/MusesDesignApplication.java
@@ -14,9 +14,9 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan("cn.muses.design")
-public class HttpsApplication {
+public class MusesDesignApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HttpsApplication.class);
+        SpringApplication.run(MusesDesignApplication.class);
     }
 }
